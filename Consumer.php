90f6c1e7351c63612109a454a867e0a0ab69781e
self-destruction <syrchikov_max@mail.ru>
2018-10-18 19:56:55 +0500

    added sockets and commandline interface

--- Consumer.php
+++ Consumer.php
@@ -4,18 +4,54 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/persisting_data.php';
 
 use App\RabbitMQ\Consumer;
+use React\EventLoop\Factory;
+use React\Socket\Server;
+use React\Socket\ConnectionInterface;
 
 $dotenv = new Dotenv\Dotenv(__DIR__ . '\config');
 $dotenv->load();
 
-$consumer = new Consumer($_ENV['RABBITMQ_HOST'], $_ENV['RABBITMQ_PORT'], $_ENV['RABBITMQ_USER'], $_ENV['RABBITMQ_PASSWORD']);
-$consumer->initChannel($_ENV['RABBITMQ_QUEUE_NAME']);
-$consumer->initQueue(function ($msg) {
-    echo " [x] Received $msg->body" . PHP_EOL;
-//    var_dump(unserialize($msg->body));
-    persistData(unserialize($msg->body));
-});
-while($consumer->countCallbacks()) {
-    $consumer->wait();
-}
-$consumer->close();
+do {
+    echo '1. Передача данных с помощью RabbitMQ.' . PHP_EOL;
+    echo '2. Передача данных с помощью сокетов.' . PHP_EOL;
+    echo '3. Выход.' . PHP_EOL;
+    echo 'Введите команду: ';
+    $line = readline();
+
+    switch ($line) {
+        case '1':
+            $consumer = new Consumer($_ENV['RABBITMQ_HOST'], $_ENV['RABBITMQ_PORT'], $_ENV['RABBITMQ_USER'], $_ENV['RABBITMQ_PASSWORD']);
+            $consumer->initChannel($_ENV['RABBITMQ_QUEUE_NAME']);
+            $consumer->initQueue(function ($msg) {
+                echo " [x] Received $msg->body" . PHP_EOL;
+//                var_dump(unserialize($msg->body));
+                persistData(unserialize($msg->body));
+            });
+            while($consumer->countCallbacks()) {
+                $consumer->wait();
+            }
+            $consumer->close();
+
+            break;
+        case '2':
+            $loop = Factory::create();
+            $socket = new Server($_ENV['SERVER_URL'], $loop);
+
+            $socket->on('connection', function (ConnectionInterface $conn) {
+
+                $conn->on('data', function ($data) use ($conn) {
+                    echo " [x] Received $data" . PHP_EOL;
+//                    var_dump(unserialize($data));
+                    persistData(unserialize($data));
+                    $conn->close();
+                });
+            });
+
+            $loop->run();
+
+            break;
+        default:
+            break;
+    }
+    echo PHP_EOL;
+} while ($line !== '3');
--- Publisher.php
+++ Publisher.php
@@ -4,16 +4,52 @@ require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/extracting_data.php';
 
 use App\RabbitMQ\Publisher;
+use React\EventLoop\Factory;
+use React\Socket\Connector;
+use React\Socket\ConnectionInterface;
 
 $dotenv = new Dotenv\Dotenv(__DIR__ . '\config');
 $dotenv->load();
 
-$publisher = new Publisher($_ENV['RABBITMQ_HOST'], $_ENV['RABBITMQ_PORT'], $_ENV['RABBITMQ_USER'], $_ENV['RABBITMQ_PASSWORD']);
-$publisher->initChannel($_ENV['RABBITMQ_QUEUE_NAME']);
-
 $data = extractData();
-$message = Publisher::createMessage(serialize($data));
-//var_dump($data);
-$publisher->send($message);
 
-$publisher->close();
+do {
+    echo '1. Передача данных с помощью RabbitMQ.' . PHP_EOL;
+    echo '2. Передача данных с помощью сокетов.' . PHP_EOL;
+    echo '3. Выход.' . PHP_EOL;
+    echo 'Введите команду: ';
+    $line = readline();
+
+    switch ($line) {
+        case '1':
+            $publisher = new Publisher($_ENV['RABBITMQ_HOST'], $_ENV['RABBITMQ_PORT'], $_ENV['RABBITMQ_USER'], $_ENV['RABBITMQ_PASSWORD']);
+            $publisher->initChannel($_ENV['RABBITMQ_QUEUE_NAME']);
+
+            $message = Publisher::createMessage(serialize($data));
+//            var_dump($data);
+            $publisher->send($message);
+
+            $publisher->close();
+
+            break;
+        case '2':
+            $loop = Factory::create();
+            $connector = new Connector($loop);
+
+            $connector->connect($_ENV['SERVER_URL'])
+                ->then(function (ConnectionInterface $conn) use ($loop, $data) {
+//                    var_dump($data);
+                    $data = serialize($data);
+                    echo " [x] Sent $data" . PHP_EOL;
+
+                    $conn->write($data);
+                });
+
+            $loop->run();
+
+            break;
+        default:
+            break;
+    }
+    echo PHP_EOL;
+} while ($line !== '3');
